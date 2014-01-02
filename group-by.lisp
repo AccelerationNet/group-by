48375c88dc9c64db3f8cfd5071d8889305c69b7d
Russ Tyndall <russ@acceleration.net>
2014-01-02 13:26:29 -0500

    explicit defgenerics to reduce warnings

--- group-by.lisp
+++ group-by.lisp
@@ -101,11 +101,12 @@ eg: (group-by '((a 1 2) (a 3 4) (b 5 6)))
   (:documentation "This class represents a list that we have grouped by multiple key values
      ala one of the group-by-repeatedly functions "))
 
-(defmethod child-groupings ((gl grouped-list))
-  (case (grouping-implementation gl)
-    (:hash-table (iter (for (k v) in-hashtable (%child-groupings gl))
-                   (collect v)))
-    (T (%child-groupings gl))))
+(defgeneric child-groupings (grouped-list)
+  (:method ((gl grouped-list))
+    (case (grouping-implementation gl)
+      (:hash-table (iter (for (k v) in-hashtable (%child-groupings gl))
+                     (collect v)))
+      (T (%child-groupings gl)))))
 
 (defun make-grouped-list (inp &key tests keys (grouping-implementation :alist))
   "Given a list of input, produce a grouped-list CLOS object that contains
@@ -160,16 +161,18 @@ of grouped-list objects
      (push item (%items node))))
   root)
 
-(defmethod add-item-to-grouping (item (gl grouped-list))
-  "puts a new item in the grouping of the grouped list (but not in the original list)"
-  (categorize-item item gl))
+(defgeneric add-item-to-grouping (item grouped-list)
+  (:method (item (gl grouped-list))
+    "puts a new item in the grouping of the grouped list (but not in the original list)"
+    (categorize-item item gl)))
 
-(defmethod %grouping-items ((gl grouped-list))
-  "Returns the items in a given group"
-  (append (%items gl)
-          (iter
-            (for cgl in (child-groupings gl))
-            (nconcing (%grouping-items cgl)))))
+(defgeneric %grouping-items (grouped-list)
+  (:method ((gl grouped-list))
+    "Returns the items in a given group"
+    (append (%items gl)
+            (iter
+              (for cgl in (child-groupings gl))
+              (nconcing (%grouping-items cgl))))))
 
 (defmethod make-child-grouped-list ((gl grouped-list) key-value grouped-list)
   (let ((c (make-instance
@@ -186,19 +189,21 @@ of grouped-list objects
       (t (push c (%child-groupings gl))))
     c))
 
-(defmethod items-in-group ((gl grouped-list) &rest key-values)
-  " a list of key values that will produce a list of all the items in a given group"
-  (let ((subgroup gl)
-        (tests (tests gl)))
-    (iter
-      (for key in key-values)
-      (for test = (or (first tests) #'equal))
-      (setf tests (rest tests))
-      (setf subgroup (find-single-sub-category subgroup key :test test)))
+(defgeneric items-in-group (grouped-list &rest keys)
+  (:documentation
+   " a list of key values that will produce a list of all the items in a given group")
+  (:method ((gl grouped-list) &rest key-values)
+    (let ((subgroup gl)
+          (tests (tests gl)))
+      (iter
+        (for key in key-values)
+        (for test = (or (first tests) #'equal))
+        (setf tests (rest tests))
+        (setf subgroup (find-single-sub-category subgroup key :test test)))
 
-    ;; Get all the items for that subgrouping (for alists this is a list we just produced)
-    ;; and that list will simply pass through
-    (%grouping-items subgroup)))
+      ;; Get all the items for that subgrouping (for alists this is a list we just produced)
+      ;; and that list will simply pass through
+      (%grouping-items subgroup))))
 
 (defun grouped-list-speed-tester (&key list keys tests hash-tests (iterations 10) actions)
   "A function to help asses which implementation will work best in your given scenario
